bb396d81f103fa8debf77bdf3d00e7a23ca21eb1
Michal Zalobny <michal.zalobny@gmail.com>
2025-01-05 11:59:41 +0100

    Add particle physics page

--- src/eleventy/debug.md
+++ src/eleventy/debug.md
@@ -2,12 +2,6 @@
 layout: 'layouts/base.njk'
 ---
 
-<div class="c-large">
-  <h1 style="font-size:100px; color:red;">Debug</h1>
-  <a style="position:fixed; left:50%; transform:translateX(-50%); top:5px; color:white" href="/">Landing</a>
-  {% include "components/footer.njk" %}
-</div>
-
 <div class="debug-holder">debug</div>
 <div class="fps">fps: <span id="fps"></span></div>
 <canvas id="canvas"></canvas>
---
+++ src/js-pages/particle-physics/utils/globalState.ts
@@ -0,0 +1,23 @@
+export const globalState = {
+  stageSize: {
+    value: [100, 100],
+  },
+  pixelRatio: {
+    value: 1,
+  },
+  slowDownFactor: {
+    value: 1,
+  },
+  uTime: {
+    value: 0,
+  },
+  mouse2DTarget: {
+    value: [0, 0],
+  },
+  mouse2DCurrent: {
+    value: [0, 0],
+  },
+  appLoadTime: 0,
+  debugHolderEl: null as HTMLDivElement | null,
+  canvasEl: null as HTMLCanvasElement | null,
+};
