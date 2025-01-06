68bb5a3a3912466dc4184f45f3194a235c7c50c2
Michal Zalobny <michal.zalobny@gmail.com>
2025-01-06 14:33:11 +0100

    Update size

--- src/js-pages/particle-physics/Components/Particle.ts
+++ src/js-pages/particle-physics/Components/Particle.ts
@@ -56,7 +56,7 @@ export class Particle {
       geometry: this.geometriesManager.getGeometry('plane'),
     });
 
-    this.mesh.scale = vec3.fromValues(100, 100, 1);
+    this.mesh.scale = vec3.fromValues(50, 50, 1);
     this.mesh.position = vec3.fromValues(this.position[0], this.position[1], 0);
     this.mesh.rotation = vec3.fromValues(0, 0, 0);
   }
