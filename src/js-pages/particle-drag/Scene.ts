3da2a2d749355e1914025ad55f29dfe7150a8626
Michal Zalobny <michal.zalobny@gmail.com>
2025-01-12 21:17:51 +0100

    Prepare for adding the impulse force

--- src/js-pages/particle-drag/Scene.ts
+++ src/js-pages/particle-drag/Scene.ts
@@ -194,31 +194,22 @@ export class Scene {
     }
   };
 
-  private onPointerDown = (e: PointerEvent) => {
-    //Create a new particle on click
-    const particle = new Particle({
-      x: e.clientX - globalState.stageSize.value[0] / 2,
-      y: -e.clientY + globalState.stageSize.value[1] / 2,
-      mass: 1 + Math.random(),
-      radius: 10 + Math.random() * 10,
-      geometriesManager: this.geometriesManager,
-      gl: this.gl,
-      camera: this.camera,
-    });
+  private onPointerDown = (e: PointerEvent) => {};
 
-    this.particles.push(particle);
-  };
+  private onPointerUp = (e: PointerEvent) => {};
 
   private addListeners() {
     window.addEventListener('keydown', this.onKeyDownWSAD);
     window.addEventListener('keyup', this.onKeyUpWSAD);
     window.addEventListener('pointerdown', this.onPointerDown);
+    window.addEventListener('pointerup', this.onPointerUp);
   }
 
   private removeListeners() {
     window.removeEventListener('keydown', this.onKeyDownWSAD);
     window.removeEventListener('keyup', this.onKeyUpWSAD);
     window.removeEventListener('pointerdown', this.onPointerDown);
+    window.removeEventListener('pointerup', this.onPointerUp);
   }
 
   public destroy() {
