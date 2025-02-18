28736972efa4d8eb6a8f1abd80c221cd1db8041d
Michal Zalobny <michal.zalobny@gmail.com>
2025-02-18 16:08:02 +0000

    Add hints

--- src/js-pages/leetcode/App.ts
+++ src/js-pages/leetcode/App.ts
@@ -1,3 +1,6 @@
+//1. Sliding window - we always iterate through right since it has to go forward, and shrink left ONLY when the constraints are not met.
+//2. The (right - left + 1) is used only to get the length of the window (length of the array) -
+//and it's always the amount of new sub arrays added after we add new component. before: [1],[1,2], [2] now: [1,X], [1,2,X], [2,X], [X] + before
 function numSubarrayProductLessThanK(nums: number[], k: number): number {
   let left = 0;
   let curr = 1;
@@ -18,6 +21,4 @@ function numSubarrayProductLessThanK(nums: number[], k: number): number {
   }
   return ans;
 }
-
 const value = numSubarrayProductLessThanK([10, 5, 2, 6], 100);
-console.log(value);
