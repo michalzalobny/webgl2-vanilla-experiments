2237b0a54d718766ef8963a87e8c17b932a2dbcf
Michal Zalobny <michal.zalobny@gmail.com>
2025-02-19 14:50:59 +0000

    Add prefix based question with solution

--- src/js-pages/leetcode/App.ts
+++ src/js-pages/leetcode/App.ts
@@ -39,4 +39,39 @@ function findBestSubarray(nums: number[], k: number): number {
 
   return ans;
 }
-console.log(findBestSubarray([3, -1, 4, 12, -8, 5, 6], 2));
+// console.log(findBestSubarray([3, -1, 4, 12, -8, 5, 6], 2));
+
+/*
+  Given an integer array nums, an array queries where queries[i] = [x, y] and an
+  integer limit, return a boolean array that represents the answer to each query. A query
+  is true if the sum of the subarray from x to y is less than limit, or false otherwise.
+*/
+function answerQueries(nums: number[], queries: number[][], limit: number): boolean[] {
+  let ans: boolean[] = [];
+  //Pre process nums array -> create prefix array, to easily get the sum of any contiguous sub array
+  // [1, 6, 3, 2, 7, 2] -> [1, 7, 10, 12, 19, 21]
+  const prefix = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    prefix.push(prefix[i - 1] + nums[i]);
+  }
+
+  for (let i = 0; i < queries.length; i++) {
+    const sub = queries[i];
+    const totalSum = prefix[sub[1]] - prefix[sub[0]] + nums[sub[0]];
+    ans.push(totalSum < limit);
+  }
+
+  return ans;
+}
+
+console.log(
+  answerQueries(
+    [1, 6, 3, 2, 7, 2],
+    [
+      [0, 3],
+      [2, 5],
+      [2, 4],
+    ],
+    13,
+  ),
+);
