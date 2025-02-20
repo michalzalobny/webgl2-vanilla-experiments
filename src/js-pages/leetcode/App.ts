676e856d0e3f0532194a919e9d13c7da1ba3724a
Michal Zalobny <michal.zalobny@gmail.com>
2025-02-20 16:56:44 +0000

    Add new problem

--- src/js-pages/leetcode/App.ts
+++ src/js-pages/leetcode/App.ts
@@ -133,5 +133,31 @@ function minStartValue(nums: number[]): number {
     return 0;
   }
 }
+// console.log(minStartValue([-3, 2, -3, 4, 2]));
 
-console.log(minStartValue([-3, 2, -3, 4, 2]));
+/*
+  You are given a 0-indexed array nums of n integers, and an integer k.
+  The k-radius average for a subarray of nums centered at some index i with the radius k is the average of all elements in nums between the indices i - k and i + k (inclusive). If there are less than k elements before or after the index i, then the k-radius average is -1.
+  Build and return an array avgs of length n where avgs[i] is the k-radius average for the subarray centered at index i.
+  The average of x elements is the sum of the x elements divided by x, using integer division. The integer division truncates toward zero, which means losing its fractional part.
+  For example, the average of four elements 2, 3, 1, and 5 is (2 + 3 + 1 + 5) / 4 = 11 / 4 = 2.75, which truncates to 2.
+*/
+function getAverages(nums: number[], k: number): number[] {
+  let runningSum = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    runningSum[i] = runningSum[i - 1] + nums[i];
+  }
+
+  let ans = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i - k < 0 || nums.length - 1 - i < k) {
+      ans[i] = -1;
+    } else {
+      let total = runningSum[i + k] - (i - k > 0 ? runningSum[i - k - 1] : 0);
+      ans[i] = Math.trunc(total / (2 * k + 1));
+    }
+  }
+  return ans;
+}
+console.log(getAverages([7, 4, 3, 9, 1, 8, 5, 2, 6], 3));
