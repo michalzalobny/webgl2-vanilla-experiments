088b31bab9171a59d1e64b39f1efbcb98c43fdef
Michal Zalobny <michal.zalobny@gmail.com>
2025-02-26 17:34:11 +0100

    Add jewels problem

--- src/js-pages/leetcode/App.ts
+++ src/js-pages/leetcode/App.ts
@@ -574,4 +574,27 @@ function canConstruct(ransomNote: string, magazine: string): boolean {
   return true;
 }
 
-console.log(canConstruct('aa', 'aab'));
+// console.log(canConstruct('aa', 'aab'));
+
+/*
+You're given strings jewels representing the types of stones that are jewels, 
+and stones representing the stones you have. Each character in stones is a type of stone you have. 
+You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+*/
+function numJewelsInStones(jewels: string, stones: string): number {
+  const types = new Set(jewels);
+  let res = 0;
+
+  for (let i = 0; i < stones.length; i++) {
+    const myStone = stones[i];
+    if (types.has(myStone)) {
+      res++;
+    }
+  }
+
+  return res;
+}
+
+console.log(numJewelsInStones('aA', 'aAAbbbb'));
