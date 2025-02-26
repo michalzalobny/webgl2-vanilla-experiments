30af17a7244a42af2f06a5451f314d9326f8997b
Michal Zalobny <michal.zalobny@gmail.com>
2025-02-26 18:57:52 +0100

    Add length of longest charstring problem

--- src/js-pages/leetcode/App.ts
+++ src/js-pages/leetcode/App.ts
@@ -597,4 +597,29 @@ function numJewelsInStones(jewels: string, stones: string): number {
   return res;
 }
 
-console.log(numJewelsInStones('aA', 'aAAbbbb'));
+// console.log(numJewelsInStones('aA', 'aAAbbbb'));
+
+/*
+  Given a string s, find the length of the longest substring without duplicate characters.
+*/
+
+function lengthOfLongestSubstring(s: string): number {
+  let charMap = new Map<string, number>();
+  let maxLength = 0;
+  let left = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    const char = s[right];
+
+    if (charMap.has(char) && charMap.get(char)! >= left) {
+      left = charMap.get(char)! + 1; // Move left pointer past the last occurrence
+    }
+
+    charMap.set(char, right);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
