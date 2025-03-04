0e7413e5dc8a83ff8e347ccf4d0b8e840197a5c2
Michal Zalobny <michal.zalobny@gmail.com>
2025-03-04 15:28:07 +0100

    Add recent counter problem

--- src/js-pages/leetcode/App.ts
+++ src/js-pages/leetcode/App.ts
@@ -812,4 +812,26 @@ var simplifyPath = function (path) {
   return '/' + result.join('/');
 };
 
-console.log(simplifyPath('/home/user/Documents/../Pictures'));
+// console.log(simplifyPath('/home/user/Documents/../Pictures'));
+
+var RecentCounter = function () {
+  this.arr = [];
+};
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function (t) {
+  this.arr.push(t);
+
+  while (this.arr[0] < t - 3000) this.arr.shift();
+
+  return this.arr.length;
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ */
+var obj = new RecentCounter();
+var param_1 = obj.ping('RecentCounter', 'ping', 'ping', 'ping', 'ping');
