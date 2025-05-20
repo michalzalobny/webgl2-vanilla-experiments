1da89dcc660f8e907a4bb9046229910fdd6fafe2
Michal Zalobny <michal.zalobny@gmail.com>
2025-05-20 06:54:52 +0200

    Add path sum binary tree problem

--- src/js-pages/leetcode/App.ts
+++ src/js-pages/leetcode/App.ts
@@ -988,3 +988,37 @@ A binary tree's maximum depth is the number of nodes along the longest path from
 //   }
 //   return ans;
 // };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+
+var hasPathSum = function (root, targetSum) {
+  let dfs = (node, curr) => {
+    if (!node) {
+      return false;
+    }
+
+    // if both children are null, then the node is a leaf
+    if (!node.left && !node.right) {
+      return curr + node.val == targetSum;
+    }
+
+    curr += node.val;
+    let left = dfs(node.left, curr);
+    let right = dfs(node.right, curr);
+    return left || right;
+  };
+
+  return dfs(root, 0);
+};
